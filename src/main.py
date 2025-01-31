292b87efb3a7ab787516b4c2fa24aaab96d00d69
arrias24 <andresarriasc@gmail.com>
2025-01-31 17:03:35 -0400

    feat: Agregado los datasets, resize a las imagenes y preparacion de los datos.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 import cv2
 import tensorflow as tf
-import keras
-import sklearn
-import pandas as pd
 import numpy as np
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import layers, models
+from tensorflow.keras.applications import MobileNetV2
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
+
+dir_dataset = "dataset-resized"
+
+datagen = ImageDataGenerator(
+    rescale=1.0/255.0,
+    validation_split=0.2
+)
 
