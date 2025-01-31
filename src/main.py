d9eaae975dc941bd0ba5dc13072de43250763038
arrias24 <andresarriasc@gmail.com>
2025-01-31 17:05:59 -0400

    feat: Generacion de datos.

--- src/main.py
+++ src/main.py
@@ -8,10 +8,31 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
+# Preparamos los datos
+
 dir_dataset = "dataset-resized"
 
 datagen = ImageDataGenerator(
     rescale=1.0/255.0,
     validation_split=0.2
 )
+
+# Generamos los datos
+
+train_generator = datagen.flow_from_directory(
+    dir_dataset,
+    target_size=(160, 160),
+    batch_size=16,
+    class_mode='categorical',
+    subset='training'
+)
+
+val_generator = datagen.flow_from_directory(
+    dir_dataset,
+    target_size=(160, 160),
+    batch_size=16,
+    class_mode='categorical',
+    subset='validation'
+)
+
 
